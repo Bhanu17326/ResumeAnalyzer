384748ea8958bb5c9cf222a7b3774db424372770
Bhanu 17326 <chintusaj@gmail.com>
2025-05-24 22:19:48 +0530

    Add main app.py file

---
+++ app.py
@@ -0,0 +1,84 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from PIL import Image
+import pdf2image
+import google.generativeai as genai
+import io
+import base64
+
+# Load environment variables
+load_dotenv()
+
+# Configure the Gemini API
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to get response from Gemini
+def get_gemini_response(input_text, pdf_content, prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content([input_text, pdf_content[0], prompt])
+    return response.text
+
+# Function to process uploaded PDF
+def input_pdf_setup(uploaded_file):
+    images = pdf2image.convert_from_bytes(
+        uploaded_file.read(),
+        poppler_path=r"C:\Users\J SAI BHANU PRAKASH\Downloads\popplers\poppler-24.08.0\Library\bin"
+    )
+    first_page = images[0]
+
+    img_byte_arr = io.BytesIO()
+    first_page.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+
+    pdf_parts = [
+        {
+            "mime_type": "image/jpeg",
+            "data": base64.b64encode(img_byte_arr).decode()
+        }
+    ]
+    return pdf_parts
+
+
+
+# STREAMLIT APP
+st.set_page_config(page_title="ATS resume Expert")
+st.header("ATS Tracking System")
+input_text=st.text_area("Job description: ",key="input")
+uploaded_file=st.file_uploader("Uploaded your resume(PDF)...",type=["pdf"])
+
+if uploaded_file is not None:
+    st.write("PDF uploaded successfully")
+
+submit1=st.button("Tell Me About the Resume")
+submit2=st.button("How Can I Improvise My Skills")
+
+submit3=st.button("Percentage Match")
+
+input_prompt1 = """
+ You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+ Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+"""
+input_prompt2 = """
+You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
+your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
+the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+if submit1:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
+elif submit3:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt2,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
